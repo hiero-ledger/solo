65a2ea23bf425c477427b2b864a006d45bbab7cc
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-05-07 08:36:59 -0500

    bump solo-chart version 0.52.0
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version.ts
+++ version.ts
@@ -12,7 +12,7 @@ import fs from 'node:fs';
 // TODO we should be consistent on the versioning format, let us drop the v prefix from the user, and manually add it
 //  right before it required, this adds better semver library compatibility
 export const HELM_VERSION: string = 'v3.14.2';
-export const SOLO_CHART_VERSION: string = '0.50.0';
+export const SOLO_CHART_VERSION: string = '0.52.0';
 export const HEDERA_PLATFORM_VERSION: string = 'v0.59.5';
 export const MIRROR_NODE_VERSION: string = 'v0.126.0';
 export const HEDERA_EXPLORER_VERSION: string = '24.12.1';
