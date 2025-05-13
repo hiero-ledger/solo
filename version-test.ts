a349b588113b095e54dd76645dbb48cd90d4f1ab
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-05-13 09:54:30 -0500

    revert
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version-test.ts
+++ version-test.ts
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
 // using a different version than the one in version.ts to test backwards compatibility
 
-export const TEST_LOCAL_HEDERA_PLATFORM_VERSION = 'v0.62.1';
+export const TEST_LOCAL_HEDERA_PLATFORM_VERSION = 'v0.58.10';
