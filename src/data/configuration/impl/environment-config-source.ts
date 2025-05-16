6d3c22b4e1be23c20e677d3e723b60e7fec5da25
Ivo Yankov <ivo@devlabs.bg>
2025-05-16 17:28:06 +0300

    feat: add EnvironmentConfigRuntimeState class
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- src/data/configuration/impl/environment-config-source.ts
+++ src/data/configuration/impl/environment-config-source.ts
@@ -2,11 +2,13 @@
 
 import {type ConfigSource} from '../spi/config-source.js';
 import {type ObjectMapper} from '../../mapper/api/object-mapper.js';
-import {LayeredConfigSource} from './layered-config-source.js';
 import {EnvironmentStorageBackend} from '../../backend/impl/environment-storage-backend.js';
 import {type Refreshable} from '../spi/refreshable.js';
 import {ConfigurationError} from '../api/configuration-error.js';
 import {Forest} from '../../key/lexer/forest.js';
+import {type EnvironmentConfigSchema} from '../../schema/model/environment/environment-config-schema.js';
+import {LayeredEnvironmentModelConfigSource} from './layered-environment-model-config-source.js';
+import {EnvironmentConfigSchemaDefinition} from '../../schema/migration/impl/environment/environment-config-schema-definition.js';
 
 /**
  * A {@link ConfigSource} that reads configuration data from the environment.
@@ -16,7 +18,10 @@ import {Forest} from '../../key/lexer/forest.js';
  * Numbers and booleans are converted from strings using the JSON parser.
  * Objects, arrays of objects, and arrays of primitives are assumed to be stored as serialized JSON strings.
  */
-export class EnvironmentConfigSource extends LayeredConfigSource implements ConfigSource, Refreshable {
+export class EnvironmentConfigSource
+  extends LayeredEnvironmentModelConfigSource<EnvironmentConfigSchema>
+  implements ConfigSource, Refreshable
+{
   /**
    * The data read from the environment.
    * @private
@@ -24,7 +29,7 @@ export class EnvironmentConfigSource extends LayeredConfigSource implements Conf
   private readonly data: Map<string, string>;
 
   public constructor(mapper: ObjectMapper, prefix?: string) {
-    super(new EnvironmentStorageBackend(prefix), mapper, prefix);
+    super(new EnvironmentConfigSchemaDefinition(mapper), new EnvironmentStorageBackend(prefix), mapper, prefix);
     this.data = new Map<string, string>();
   }
 
@@ -40,7 +45,7 @@ export class EnvironmentConfigSource extends LayeredConfigSource implements Conf
     await this.load();
   }
 
-  public async load(): Promise<void> {
+  public override async load(): Promise<void> {
     this.data.clear();
     this.forest = undefined;
 
