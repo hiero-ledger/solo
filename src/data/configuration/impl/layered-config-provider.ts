ca9a9660787327ee2ca81d9f2b5c7b4457135990
Ivo Yankov <ivo@devlabs.bg>
2025-05-20 12:31:38 +0300

    feat: solo runtime state (#1982)
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    Co-authored-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/data/configuration/impl/layered-config-provider.ts
+++ src/data/configuration/impl/layered-config-provider.ts
@@ -6,19 +6,16 @@ import {type Config} from '../api/config.js';
 import {LayeredConfigBuilder} from './layered-config-builder.js';
 import {InjectTokens} from '../../../core/dependency-injection/inject-tokens.js';
 import {type ObjectMapper} from '../../mapper/api/object-mapper.js';
-import {inject} from 'tsyringe-neo';
-import {patchInject} from '../../../core/dependency-injection/container-helper.js';
 import {IllegalArgumentError} from '../../../business/errors/illegal-argument-error.js';
 import {ConfigurationError} from '../api/configuration-error.js';
+import {container} from 'tsyringe-neo';
 
 export class LayeredConfigProvider implements ConfigProvider {
   private _config: Config | undefined;
+  private readonly mapper: ObjectMapper;
 
-  public constructor(
-    @inject(InjectTokens.ObjectMapper) private readonly mapper: ObjectMapper,
-    private readonly prefix?: string,
-  ) {
-    this.mapper = patchInject(mapper, InjectTokens.ObjectMapper, LayeredConfigProvider.name);
+  public constructor(private readonly prefix?: string) {
+    this.mapper = container.resolve(InjectTokens.ObjectMapper);
   }
 
   public builder(): ConfigBuilder {
