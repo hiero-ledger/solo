f27361b81c77562819800399644e62e1068d7fc7
Ivo Yankov <ivo@devlabs.bg>
2025-05-20 12:31:38 +0300

    feat: solo runtime state (#1982)
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>
    Co-authored-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/data/configuration/impl/environment-model-config-source.ts
+++ src/data/configuration/impl/environment-model-config-source.ts
@@ -3,9 +3,9 @@
 import {type SchemaDefinition} from '../../schema/migration/api/schema-definition.js';
 import {type ObjectMapper} from '../../mapper/api/object-mapper.js';
 import {type EnvironmentStorageBackend} from '../../backend/impl/environment-storage-backend.js';
-import {LayeredEnvironmentModelConfigSource} from './layered-environment-model-config-source.js';
+import {LayeredSoloModelConfigSource} from './layered-solo-model-config-source.js';
 
-export abstract class EnvironmentModelConfigSource<T extends object> extends LayeredEnvironmentModelConfigSource<T> {
+export abstract class EnvironmentModelConfigSource<T extends object> extends LayeredSoloModelConfigSource<T> {
   protected constructor(
     schema: SchemaDefinition<T>,
     backend: EnvironmentStorageBackend,
