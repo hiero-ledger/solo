f2c01820dd7e2a39a2dbbc7ade7cc83796f33d01
Ivo Yankov <ivo@devlabs.bg>
2025-05-21 13:05:44 +0300

    chore: add util unit tests
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- src/core/dependency-injection/inject-tokens.ts
+++ src/core/dependency-injection/inject-tokens.ts
@@ -58,4 +58,5 @@ export const InjectTokens = {
   LocalConfigSource: Symbol.for('LocalConfigSource'),
   LocalConfigRuntimeState: Symbol.for('LocalConfigRuntimeState'),
   HomeDirectory: Symbol.for('HomeDirectory'),
+  SoloConfigPrefix: Symbol.for('SoloConfigPrefix'),
 };
