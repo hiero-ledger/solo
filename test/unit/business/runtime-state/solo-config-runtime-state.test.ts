31511e3685d55aac88df8afa4179873f486e174b
Ivo Yankov <ivo@devlabs.bg>
2025-05-21 14:51:58 +0300

    chore: cleanup and unit tests
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- test/unit/business/runtime-state/solo-config-runtime-state.test.ts
+++ test/unit/business/runtime-state/solo-config-runtime-state.test.ts
@@ -34,32 +34,73 @@ describe('SoloConfigRuntimeState', (): void => {
     );
   });
 
-  it('should load environment variables into solo state', async (): Promise<void> => {
-    const directory: string = '../solo-charts/charts';
-    process.env['SOLO_HELM-CHART_DIRECTORY'] = directory;
-    await soloConfigRuntimeState.load();
-    const soloConfig: SoloConfig = soloConfigRuntimeState.soloConfig;
-    expect(soloConfig).to.have.property('helmChart');
-    expect(soloConfig.helmChart).to.have.property('directory');
-    expect(soloConfig.helmChart.directory).to.equal(directory);
-    expect(soloConfig.helmChart.repository).to.equal(directory);
-    delete process.env.SOLO_SC_HELMCHART_DIRECTORY;
-  });
+  describe('EnvironmentRuntimeState', (): void => {
+    let initialEnvironment: any;
+    beforeEach((): void => {
+      // create a clone to avoid mutation
+      initialEnvironment = Object.assign({}, process.env);
+    });
 
-  it('should overwrite runtime state with environment data', async (): Promise<void> => {
-    await soloConfigRuntimeState.load();
-    expect(soloConfigRuntimeState.soloConfig).to.have.property('helmChart');
-    expect(soloConfigRuntimeState.soloConfig.helmChart).to.have.property('repository');
-    expect(soloConfigRuntimeState.soloConfig.helmChart.repository).to.equal('oci://ghcr.io/hashgraph/solo-charts');
+    afterEach((): void => {
+      // Reset the environment variables to their initial state
+      process.env = initialEnvironment;
+    });
 
-    const overwrittenHelmChartRepository: string = 'oci://ghcr.io/overwritten/charts';
-    process.env.SOLO_SC_HELMCHART_REPOSITORY = overwrittenHelmChartRepository;
-    await soloConfigRuntimeState.load();
+    it('should load environment variables into solo state', async (): Promise<void> => {
+      const directory: string = '../solo-charts/charts';
+      process.env['SOLO_HELM-CHART_DIRECTORY'] = directory;
+      await soloConfigRuntimeState.load();
+      const soloConfig: SoloConfig = soloConfigRuntimeState.soloConfig;
+      expect(soloConfig).to.have.property('helmChart');
+      expect(soloConfig.helmChart).to.have.property('directory');
+      expect(soloConfig.helmChart.directory).to.equal(directory);
+      expect(soloConfig.helmChart.repository).to.equal(directory);
+    });
 
-    expect(soloConfigRuntimeState.soloConfig).to.have.property('helmChart');
-    expect(soloConfigRuntimeState.soloConfig.helmChart).to.have.property('repository');
-    expect(soloConfigRuntimeState.soloConfig.helmChart.repository).to.equal(overwrittenHelmChartRepository);
-    delete process.env.SOLO_SC_HELMCHART_REPOSITORY;
+    it('should overwrite runtime state with value from environment variable', async (): Promise<void> => {
+      await soloConfigRuntimeState.load();
+      expect(soloConfigRuntimeState.soloConfig).to.have.property('helmChart');
+      expect(soloConfigRuntimeState.soloConfig.helmChart).to.have.property('repository');
+      expect(soloConfigRuntimeState.soloConfig.helmChart.repository).to.equal('oci://ghcr.io/hashgraph/solo-charts');
+
+      const overwrittenHelmChartRepository: string = 'oci://ghcr.io/overwritten/charts';
+      process.env['SOLO_HELM-CHART_DIRECTORY'] = overwrittenHelmChartRepository;
+      await soloConfigRuntimeState.load();
+
+      expect(soloConfigRuntimeState.soloConfig).to.have.property('helmChart');
+      expect(soloConfigRuntimeState.soloConfig.helmChart).to.have.property('repository');
+      expect(soloConfigRuntimeState.soloConfig.helmChart.repository).to.equal(overwrittenHelmChartRepository);
+    });
+
+    it('should merge multiple configuration sources correctly', async (): Promise<void> => {
+      process.env['SOLO_HELM-CHART_NAME'] = 'merged-deployment';
+      process.env['SOLO_HELM-CHART_REPOSITORY'] = 'oci://ghcr.io/merged/charts';
+
+      await soloConfigRuntimeState.load();
+      const soloConfig: SoloConfig = soloConfigRuntimeState.soloConfig;
+
+      expect(soloConfig.helmChart).to.be.an('object');
+      expect(soloConfig.helmChart.name).to.equal('merged-deployment');
+      expect(soloConfig.helmChart.repository).to.equal('oci://ghcr.io/merged/charts');
+    });
+
+    it('should overwrite nodes in multiple config trees', async (): Promise<void> => {
+      process.env['SOLO_HELM-CHART_NAME'] = 'merged-helm-chart-deployment';
+      process.env['SOLO_HELM-CHART_REPOSITORY'] = 'oci://ghcr.io/merged/helm/charts';
+      process.env['SOLO_CLUSTER-SETUP-HELM-CHART_NAME'] = 'merged-cluster-setup-helm-chart-name';
+      process.env['SOLO_CLUSTER-SETUP-HELM-CHART_REPOSITORY'] = 'oci://ghcr.io/merged/cluster/setup/helm/charts';
+
+      await soloConfigRuntimeState.load();
+      const soloConfig: SoloConfig = soloConfigRuntimeState.soloConfig;
+
+      expect(soloConfig.helmChart).to.be.an('object');
+      expect(soloConfig.helmChart.name).to.equal('merged-helm-chart-deployment');
+      expect(soloConfig.helmChart.repository).to.equal('oci://ghcr.io/merged/helm/charts');
+
+      expect(soloConfig.clusterSetupHelmChart).to.be.an('object');
+      expect(soloConfig.clusterSetupHelmChart.name).to.equal('merged-cluster-setup-helm-chart-name');
+      expect(soloConfig.clusterSetupHelmChart.repository).to.equal('oci://ghcr.io/merged/cluster/setup/helm/charts');
+    });
   });
 
   describe('EnvironmentRuntimeState', (): void => {
