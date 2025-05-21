fce1d82cec2e25f456f59254f00fe008f7b8a5bd
Jeromy Cannon <jeromy@swirldslabs.com>
2025-05-21 07:32:24 +0100

    fix: reorder imports in layered-config-provider for better organization
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/data/configuration/impl/layered-config-provider.ts
+++ src/data/configuration/impl/layered-config-provider.ts
@@ -6,8 +6,8 @@ import {type Config} from '../api/config.js';
 import {LayeredConfigBuilder} from './layered-config-builder.js';
 import {InjectTokens} from '../../../core/dependency-injection/inject-tokens.js';
 import {type ObjectMapper} from '../../mapper/api/object-mapper.js';
-import {patchInject} from '../../../core/dependency-injection/container-helper.js';
 import {inject, injectable} from 'tsyringe-neo';
+import {patchInject} from '../../../core/dependency-injection/container-helper.js';
 import {IllegalArgumentError} from '../../../business/errors/illegal-argument-error.js';
 import {ConfigurationError} from '../api/configuration-error.js';
 
