f3810c76be23cf3f3dd6fd2fcede1b17e8099e8e
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-05-27 14:42:29 -0500

    test after migration
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -33,7 +33,7 @@ solo init
 solo cluster setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
 solo node keys --gossip-keys --tls-keys -i node1,node2
 solo deployment create -i node1,node2 -n "${SOLO_NAMESPACE}" --context kind-"${SOLO_CLUSTER_NAME}" --email john@doe.com --deployment-clusters kind-"${SOLO_CLUSTER_NAME}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --deployment "${SOLO_DEPLOYMENT}"
-solo network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
+solo network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs
 
 solo node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
 solo node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
@@ -66,6 +66,12 @@ if ! grep -q "schemaVersion: 1" ./remote-config-after.yaml; then
   echo "schemaVersion: 1 not found in remote-config-after.yaml"
   exit 1
 fi
+
+npm run solo-test -- mirror-node deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --pinger -q --dev
+
+npm run solo-test -- explorer deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --mirrorNamespace solo-e2e -q --dev
+
+.github/workflows/script/solo_smoke_test.sh
 
 # uninstall components using current version
 npm run solo-test -- explorer destroy --deployment "${SOLO_DEPLOYMENT}" --force
