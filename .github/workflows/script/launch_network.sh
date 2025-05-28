a0c27f47dc17fe6accf843b8c2b24d384f246cfd
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-05-28 09:26:48 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -20,7 +20,7 @@ solo --version
 SOLO_CLUSTER_NAME=solo-e2e
 SOLO_NAMESPACE=solo-e2e
 SOLO_CLUSTER_SETUP_NAMESPACE=solo-setup
-SOLO_DEPLOYMENT=solo-e2e
+export SOLO_DEPLOYMENT=solo-e2e
 
 kind delete cluster -n "${SOLO_CLUSTER_NAME}"
 kind create cluster -n "${SOLO_CLUSTER_NAME}"
