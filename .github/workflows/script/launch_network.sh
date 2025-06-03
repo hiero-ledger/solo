ec741e7c0a2e72891014f06a1047f2b81815e75d
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-06-03 00:38:22 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -14,7 +14,7 @@ then
     exit 1
 fi
 
-npm install -g @hashgraph/solo@"${releaseTag}" --force
+#npm install -g @hashgraph/solo@"${releaseTag}" --force
 solo --version
 
 export SOLO_CLUSTER_NAME=solo-e2e
@@ -30,17 +30,33 @@ rm -rf ~/.solo/*
 
 echo "Launch solo using released Solo version ${releaseTag}"
 
-export CONSENSUS_NODE_VERSION=v0.58.10 #$(grep 'HEDERA_PLATFORM_VERSION' version.ts | sed -E "s/.*'([^']+)';/\1/")
 
 solo init
 solo cluster setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
 solo node keys --gossip-keys --tls-keys -i node1,node2
 solo deployment create -i node1,node2 -n "${SOLO_NAMESPACE}" --context kind-"${SOLO_CLUSTER_NAME}" --email john@doe.com --deployment-clusters kind-"${SOLO_CLUSTER_NAME}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --deployment "${SOLO_DEPLOYMENT}"
-solo network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q
 
+# Migrate step by step from v0.58.10 to v0.59.5 and then to v0.60.1
+export CONSENSUS_NODE_VERSION=v0.58.10
+solo network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q
 solo node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
 solo node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
 solo account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
+solo node stop -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
+
+export CONSENSUS_NODE_VERSION=v0.59.5
+solo network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q || true
+solo node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
+solo node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
+solo account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
+solo node stop -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
+
+export CONSENSUS_NODE_VERSION=v0.60.1
+solo network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q || true
+solo node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
+solo node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
+solo account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
+solo node logs -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
 
 solo mirror-node deploy  --deployment "${SOLO_DEPLOYMENT}"
 solo explorer deploy -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
@@ -51,8 +67,8 @@ cat ./local-config-before.yaml
 kubectl get ConfigMap solo-remote-config -n ${SOLO_NAMESPACE} -o yaml | yq '.data' > remote-config-before.yaml
 cat remote-config-before.yaml
 
-# must uninstall explorer before migration, because the change of explorer chart name and labels make it harder to uninstall
-# or upgrade after migration
+# must uninstall explorer before migration, because the change of explorer chart name and labels
+# make it harder to uninstall or upgrade after migration
 solo explorer destroy --deployment "${SOLO_DEPLOYMENT}" --force
 
 # trigger migration
@@ -75,8 +91,6 @@ if ! grep -q "schemaVersion: 1" ./remote-config-after.yaml; then
   exit 1
 fi
 
-# npm run solo-test -- node stop -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
-
 # redeploy mirror-node to upgrade to a newer version
 npm run solo-test -- mirror-node deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --pinger -q --dev
 
@@ -84,11 +98,10 @@ npm run solo-test -- mirror-node deploy --deployment "${SOLO_DEPLOYMENT}" --clus
 npm run solo-test -- explorer deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --mirrorNamespace solo-e2e -q --dev
 
 # using new solo to redeploy solo deployment chart to new version
-#kubectl delete statefulset network-node1 network-node2 --cascade=orphan -n "${SOLO_NAMESPACE}"
-#npm run solo-test -- network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q
-#npm run solo-test -- node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
-#npm run solo-test -- node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
-
+kubectl delete statefulset network-node1 network-node2 --cascade=orphan -n "${SOLO_NAMESPACE}"
+npm run solo-test -- network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q
+npm run solo-test -- node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
+npm run solo-test -- node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
 
 kubectl port-forward -n "${SOLO_NAMESPACE}" svc/haproxy-node1-svc 50211:50211 > /dev/null 2>&1 &
 kubectl port-forward -n "${SOLO_NAMESPACE}" svc/relay-node1-node2-hedera-json-rpc-relay 7546:7546 > /dev/null 2>&1 &
@@ -102,9 +115,9 @@ ps -ef |grep port-forward
 npm run solo-test -- account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
 
 # uninstall components using current version
-
-npm run solo-test -- explorer destroy --deployment "${SOLO_DEPLOYMENT}" --force
-npm run solo-test -- relay destroy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
-npm run solo-test -- mirror-node destroy --deployment "${SOLO_DEPLOYMENT}" --force
-npm run solo-test -- node stop -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
-npm run solo-test -- network destroy --deployment "${SOLO_DEPLOYMENT}" --force
+#
+#npm run solo-test -- explorer destroy --deployment "${SOLO_DEPLOYMENT}" --force
+#npm run solo-test -- relay destroy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
+#npm run solo-test -- mirror-node destroy --deployment "${SOLO_DEPLOYMENT}" --force
+#npm run solo-test -- node stop -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
+#npm run solo-test -- network destroy --deployment "${SOLO_DEPLOYMENT}" --force
