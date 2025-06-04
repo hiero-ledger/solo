6b25de1b85349b708ad6ad64e18f346edf3c9d72
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-06-04 00:11:50 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -43,9 +43,14 @@ solo node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
 solo account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
 #solo node stop -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
 
+kubectl get secret uploader-mirror-secrets -o jsonpath='{.data.S3_ACCESS_KEY}' -n solo-e2e
+kubectl get secret minio-secrets -o jsonpath='{.data}' -n solo-e2e
+
 solo mirror-node deploy  --deployment "${SOLO_DEPLOYMENT}"
 # solo explorer deploy -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
-solo relay deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
+#solo relay deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
+
+kubectl exec mirror-redis-node-0 -c redis -n solo-e2e -- env
 
 cp ~/.solo/cache/local-config.yaml ./local-config-before.yaml
 cat ./local-config-before.yaml
@@ -76,31 +81,43 @@ if ! grep -q "schemaVersion: 1" ./remote-config-after.yaml; then
   exit 1
 fi
 
+
+
+
+
 # using new solo to redeploy solo deployment chart to new version
-kubectl delete statefulset network-node1 network-node2 --cascade=orphan -n "${SOLO_NAMESPACE}"
+#kubectl delete statefulset network-node1 network-node2 --cascade=orphan -n "${SOLO_NAMESPACE}"
 npm run solo-test -- network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q --settings-txt ./settings.txt
+
+kubectl get secret uploader-mirror-secrets -o jsonpath='{.data.S3_ACCESS_KEY}' -n solo-e2e
+kubectl get secret minio-secrets -o jsonpath='{.data}' -n solo-e2e
 
 # must kill the pods to trigger redeployment to pickup changes to new image of root-container
 # otherwise the missing of zip/unzip would make the platform extraction on nodes fail
 kubectl delete pod network-node1-0 -n solo-e2e
 kubectl delete pod network-node2-0 -n solo-e2e
 
+kubectl get secret uploader-mirror-secrets -o jsonpath='{.data.S3_ACCESS_KEY}' -n solo-e2e
+kubectl get secret minio-secrets -o jsonpath='{.data}' -n solo-e2e
+
 npm run solo-test -- node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
 npm run solo-test -- node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
-
-kubectl port-forward -n "${SOLO_NAMESPACE}" svc/haproxy-node1-svc 50211:50211 > /dev/null 2>&1 &
-kubectl port-forward -n "${SOLO_NAMESPACE}" svc/relay-node1-node2-hedera-json-rpc-relay 7546:7546 > /dev/null 2>&1 &
-kubectl port-forward -n "${SOLO_NAMESPACE}" svc/mirror-grpc 5600:5600 > /dev/null 2>&1 &
-kubectl port-forward -n "${SOLO_NAMESPACE}" svc/hiero-explorer 8080:80 > /dev/null 2>&1 &
-kubectl port-forward -n "${SOLO_NAMESPACE}" svc/mirror-rest 5551:80 > /dev/null 2>&1 &
 
 ps -ef |grep port-forward
 
 # redeploy mirror-node to upgrade to a newer version
 npm run solo-test -- mirror-node deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --pinger -q --dev
 
+kubectl exec mirror-redis-node-0 -c redis -n solo-e2e -- env
+
 # redeploy explorer
-npm run solo-test -- explorer deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --mirrorNamespace solo-e2e -q --dev
+#npm run solo-test -- explorer deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --mirrorNamespace solo-e2e -q --dev
+
+kubectl port-forward -n "${SOLO_NAMESPACE}" svc/haproxy-node1-svc 50211:50211 > /dev/null 2>&1 &
+kubectl port-forward -n "${SOLO_NAMESPACE}" svc/relay-node1-node2-hedera-json-rpc-relay 7546:7546 > /dev/null 2>&1 &
+kubectl port-forward -n "${SOLO_NAMESPACE}" svc/mirror-grpc 5600:5600 > /dev/null 2>&1 &
+kubectl port-forward -n "${SOLO_NAMESPACE}" svc/hiero-explorer 8080:80 > /dev/null 2>&1 &
+kubectl port-forward -n "${SOLO_NAMESPACE}" svc/mirror-rest 5551:80 > /dev/null 2>&1 &
 
 # Test transaction can still be sent and processed
 npm run solo-test -- account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
