7dd8dfeb0ac1983cc21f02ed434b39cc43e6230e
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-06-04 15:46:53 -0500

    uninstall
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -110,9 +110,8 @@ npm run solo-test -- account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amo
 .github/workflows/script/solo_smoke_test.sh
 
 # uninstall components using current Solo version
-#
-#npm run solo-test -- explorer destroy --deployment "${SOLO_DEPLOYMENT}" --force
-#npm run solo-test -- relay destroy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
-#npm run solo-test -- mirror-node destroy --deployment "${SOLO_DEPLOYMENT}" --force
-#npm run solo-test -- node stop -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
-#npm run solo-test -- network destroy --deployment "${SOLO_DEPLOYMENT}" --force
+npm run solo-test -- explorer destroy --deployment "${SOLO_DEPLOYMENT}" --force
+npm run solo-test -- relay destroy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
+npm run solo-test -- mirror-node destroy --deployment "${SOLO_DEPLOYMENT}" --force
+npm run solo-test -- node stop -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
+npm run solo-test -- network destroy --deployment "${SOLO_DEPLOYMENT}" --force
