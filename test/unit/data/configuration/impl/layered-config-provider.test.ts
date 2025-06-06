d81cb51bf730f73168546eec091456da9f05e022
Nathan Klick <nathan@swirldslabs.com>
2025-06-06 11:47:14 -0500

    fix: config provider register method throws inappropriately (#2095)
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

---
+++ test/unit/data/configuration/impl/layered-config-provider.test.ts
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import {type ConfigProvider} from '../../../../../src/data/configuration/api/config-provider.js';
+import {type Config} from '../../../../../src/data/configuration/api/config.js';
+import {LayeredConfigProvider} from '../../../../../src/data/configuration/impl/layered-config-provider.js';
+import {ClassToObjectMapper} from '../../../../../src/data/mapper/impl/class-to-object-mapper.js';
+import {ConfigKeyFormatter} from '../../../../../src/data/key/config-key-formatter.js';
+import {expect} from 'chai';
+import {ConfigurationError} from '../../../../../src/data/configuration/api/configuration-error.js';
+
+describe('LayeredConfigProvider', (): void => {
+  const mapper: ClassToObjectMapper = new ClassToObjectMapper(ConfigKeyFormatter.instance());
+
+  it('should not throw an error when registering a valid initial config (issue #2094)', (): void => {
+    const provider: ConfigProvider = new LayeredConfigProvider(mapper, 'SOLO');
+    expect(provider).is.not.null.and.not.undefined;
+    expect((): Config => provider.config()).to.throw(ConfigurationError);
+
+    const config: Config = provider.builder().build();
+    expect((): Config => provider.config()).to.throw(ConfigurationError);
+
+    expect((): void => provider.register(config)).to.not.throw();
+    expect((): Config => provider.config()).to.not.throw();
+  });
+});
