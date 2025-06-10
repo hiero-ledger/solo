c037146870df065185683e7857bf5601556ee7f6
Jeromy Cannon <jeromy@swirldslabs.com>
2025-06-10 19:52:30 +0100

    fix: refactor get-block.sh to work with block-node v0.10+
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/data/get-block.sh
+++ test/data/get-block.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-usage_error() {
-  echo "Usage: $0 <integer>"
-  exit 1
-}
+PROTO_DIR=$(dirname "$(realpath $0)")
 
-# An integer is expected as the first parameter
-if [ "$#" -lt 1 ] || ! [[ "$1" =~ ^[0-9]+$ ]]; then
-  usage_error
-fi
+# block-access, getBlock --> should work as is.
+grpcurl -plaintext \
+  -import-path "$PROTO_DIR" \
+  -proto "$PROTO_DIR/block_access_service.proto" \
+  -d '{"retrieveLatest": true}' \
+  localhost:8080 \
+  org.hiero.block.api.BlockAccessService/getBlock
 
-# If the script reaches here, the parameters are valid
-echo "Param is: $1"
-
-# Use environment variables or default values
-GRPC_SERVER=${GRPC_SERVER:-"localhost:8080"}
-GRPC_METHOD=${GRPC_METHOD:-"com.hedera.hapi.block.BlockAccessService/singleBlock"}
-PATH_TO_PROTO="./block_service.proto"
-
-echo "Requesting block $1..."
-
-# Response block messages from the gRPC server are printed to stdout.
-echo "{\"block_number\": $1}" | grpcurl -plaintext -proto $PATH_TO_PROTO -d @ $GRPC_SERVER $GRPC_METHOD
+# subscriber, needs a fix before working.
+grpcurl -plaintext \
+  -import-path "$PROTO_DIR" \
+  -proto "$PROTO_DIR/block_stream_subscribe_service.proto" \
+  -d '{"startBlockNumber": 0, "endBlockNumber": 10}' \
+  localhost:8080 \
+  org.hiero.block.api.BlockStreamSubscribeService/subscribeBlockStream
