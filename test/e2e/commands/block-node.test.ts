ae071a752ea8d87fdf76671d144606c7c9e997b2
Jeromy Cannon <jeromy@swirldslabs.com>
2025-06-11 13:45:28 +0100

    feat: import NetworkNodes type in block-node test for enhanced type safety
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- test/e2e/commands/block-node.test.ts
+++ test/e2e/commands/block-node.test.ts
@@ -9,6 +9,7 @@ import * as version from '../../../version.js';
 import {sleep} from '../../../src/core/helpers.js';
 import {Duration} from '../../../src/core/time/duration.js';
 import {NamespaceName} from '../../../src/types/namespace/namespace-name.js';
+import {type NetworkNodes} from '../../../src/core/network-nodes.js';
 import {container} from 'tsyringe-neo';
 import {InjectTokens} from '../../../src/core/dependency-injection/inject-tokens.js';
 import {Argv} from '../../helpers/argv-wrapper.js';
