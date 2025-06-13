2776229072a8ea916549db67bfae257cc5da7312
Jeromy Cannon <jeromy@swirldslabs.com>
2025-06-13 17:52:13 +0100

    fix: clean up update_md.sh and wrapper_for_update_md.sh by removing unnecessary commands and improving script execution
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- docs/site/wrapper_for_update_md.sh
+++ docs/site/wrapper_for_update_md.sh
@@ -20,38 +20,9 @@ solo --version
 node -p -e "Boolean(process.stdout.isTTY)"
 chmod 755 ./.github/workflows/script/update_md.sh
 
-cd docs/
-pwd
-
-../.github/workflows/script/update_md.sh </dev/null | cat
+.github/workflows/script/update_md.sh </dev/null | cat
 set +x
-echo "::group::Updated step-by-step-guide.md"
 
-cd ..
+echo "::group::Updated step-by-step-guide.md"
 cat docs/site/content/en/docs/step-by-step-guide.md
 echo "::endgroup::"
-
-set +e
-echo "::group::Git Diff step-by-step-guide.md"
-git diff --stat
-echo "::endgroup::"
-
-CHANGES=$(git diff --stat)
-echo "Changes: $CHANGES"
-
-INSERTIONS=$(echo $CHANGES | grep -o -P '(?<=insertions\(\+\), )\d+')
-echo "Insertions: $INSERTIONS"
-
-DELETIONS=$(echo $CHANGES | grep -o '[0-9]\+' | tail -1)
-echo "Deletions: $DELETIONS"
-
-# Calculate total lines changed if INSERTIONS and DELETIONS are not empty
-if [ -z "$INSERTIONS" ]; then
-  INSERTIONS=0
-fi
-if [ -z "$DELETIONS" ]; then
-    DELETIONS=0
-fi
-
-TOTAL_LINES_CHANGED=$(($INSERTIONS + $DELETIONS))
-echo "Total step-by-step-guide.md lines changed: $TOTAL_LINES_CHANGED"
