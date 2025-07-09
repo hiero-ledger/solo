91900f167c5a83dc4f9547650f70f04bb14ced79
Jeromy Cannon <jeromy@swirldslabs.com>
2025-07-09 17:25:54 +0100

    fix: update launch script for mirror node deployment and adjust consensus node version
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -32,11 +32,20 @@ echo "Launch solo using released Solo version ${releaseTag}"
 
 
 solo init
-solo cluster setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
-solo node keys --gossip-keys --tls-keys
-solo deployment create -i node1,node2 -n "${SOLO_NAMESPACE}" --context kind-"${SOLO_CLUSTER_NAME}" --email john@doe.com --deployment-clusters kind-"${SOLO_CLUSTER_NAME}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --deployment "${SOLO_DEPLOYMENT}"
+solo cluster-ref connect --cluster-ref kind-${SOLO_CLUSTER_NAME} --context kind-${SOLO_CLUSTER_NAME}
+solo deployment create -n "${SOLO_NAMESPACE}" --deployment "${SOLO_DEPLOYMENT}"
+solo deployment add-cluster --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --num-consensus-nodes 2
+solo node keys --gossip-keys --tls-keys --deployment "${SOLO_DEPLOYMENT}"
+solo cluster-ref setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
+solo network deploy --deployment "${SOLO_DEPLOYMENT}" "${CONSENSUS_NODE_FLAG[@]}"
+solo node setup --deployment "${SOLO_DEPLOYMENT}" "${CONSENSUS_NODE_FLAG[@]}"
+solo node start --deployment "${SOLO_DEPLOYMENT}"
+solo mirror-node deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} -q
+solo explorer deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} -q
+solo cluster-ref setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
+solo relay deploy -i node1 --deployment "${SOLO_DEPLOYMENT}"
 
-export CONSENSUS_NODE_VERSION=v0.58.10
+export CONSENSUS_NODE_VERSION=v0.60.1
 # Use custom settings file for the deployment to avoid too many state saved in disk causing the no space left on device error
 solo network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q --settings-txt .github/workflows/support/v58-test/settings.txt
 solo node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
