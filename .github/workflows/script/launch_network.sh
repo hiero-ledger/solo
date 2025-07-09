bb605148588cce458266bf33c70f2d878a9b4d02
Jeromy Cannon <jeromy@swirldslabs.com>
2025-07-09 22:37:33 +0100

    fix: update launch script for mirror node deployment and adjust cluster setup
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -32,18 +32,9 @@ echo "Launch solo using released Solo version ${releaseTag}"
 
 
 solo init
-solo cluster-ref connect --cluster-ref kind-${SOLO_CLUSTER_NAME} --context kind-${SOLO_CLUSTER_NAME}
-solo deployment create -n "${SOLO_NAMESPACE}" --deployment "${SOLO_DEPLOYMENT}"
-solo deployment add-cluster --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --num-consensus-nodes 2
-solo node keys --gossip-keys --tls-keys --deployment "${SOLO_DEPLOYMENT}"
-solo cluster-ref setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
-solo network deploy --deployment "${SOLO_DEPLOYMENT}" "${CONSENSUS_NODE_FLAG[@]}"
-solo node setup --deployment "${SOLO_DEPLOYMENT}" "${CONSENSUS_NODE_FLAG[@]}"
-solo node start --deployment "${SOLO_DEPLOYMENT}"
-solo mirror-node deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} -q
-solo explorer deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} -q
-solo cluster-ref setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
-solo relay deploy -i node1 --deployment "${SOLO_DEPLOYMENT}"
+solo cluster setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"	solo cluster-ref connect --cluster-ref kind-${SOLO_CLUSTER_NAME} --context kind-${SOLO_CLUSTER_NAME}
+solo node keys --gossip-keys --tls-keys -i node1,node2	solo deployment create -n "${SOLO_NAMESPACE}" --deployment "${SOLO_DEPLOYMENT}"
+solo deployment create -i node1,node2 -n "${SOLO_NAMESPACE}" --context kind-"${SOLO_CLUSTER_NAME}" --email john@doe.com --deployment-clusters kind-"${SOLO_CLUSTER_NAME}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --deployment "${SOLO_DEPLOYMENT}"
 
 export CONSENSUS_NODE_VERSION=v0.58.10
 # Use custom settings file for the deployment to avoid too many state saved in disk causing the no space left on device error
@@ -53,7 +44,7 @@ solo node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
 solo account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
 
 
-solo mirror-node deploy  --deployment "${SOLO_DEPLOYMENT}" --enable-ingress --pinger
+solo mirror-node deploy  --deployment "${SOLO_DEPLOYMENT}" --pinger
 solo explorer deploy -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
 solo relay deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
 
@@ -95,7 +86,7 @@ npm run solo-test -- node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
 npm run solo-test -- node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
 
 # redeploy mirror-node to upgrade to a newer version
-npm run solo-test -- mirror-node deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --pinger -q --dev
+npm run solo-test -- mirror-node deploy --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --enable-ingress --pinger -q --dev
 
 # redeploy explorer and relay node to upgrade to a newer version
 npm run solo-test -- relay deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q --dev
