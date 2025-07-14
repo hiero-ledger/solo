90d3c8cae0cb512e34e305fa3ed507855a1709a6
Jeromy Cannon <jeromy@swirldslabs.com>
2025-07-14 18:06:12 +0100

    refactor: remove wtfnode logger integration from solo.ts
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- solo.ts
+++ solo.ts
@@ -8,24 +8,13 @@ import {type SoloLogger} from './src/core/logging/solo-logger.js';
 import {InjectTokens} from './src/core/dependency-injection/inject-tokens.js';
 import {container} from 'tsyringe-neo';
 import {type ErrorHandler} from './src/core/error-handler.js';
-import * as wtf from 'wtfnode';
 
 const context: {logger: SoloLogger} = {logger: undefined};
 
 await fnm
   .main(process.argv, context)
   .then((): void => {
-    wtf.setLogger('info', (message: string): void => {
-      context.logger.info(message);
-    });
-    wtf.setLogger('warn', (message: string): void => {
-      context.logger.warn(message);
-    });
-    wtf.setLogger('error', (message: string): void => {
-      context.logger.error(message);
-    });
     context.logger.info('Solo CLI completed, via entrypoint');
-    wtf.dump({fullStacks: true}); // Dump the active handles and requests
   })
   .catch((error): void => {
     const errorHandler: ErrorHandler = container.resolve(InjectTokens.ErrorHandler);
