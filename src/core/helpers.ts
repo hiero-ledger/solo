bea11dff2090349ead324c04a51f3a6ec520ff6b
Jeromy Cannon <jeromy@swirldslabs.com>
2025-07-14 18:01:10 +0100

    refactor: add TODO comments for logger enhancements in tasks and helpers
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/core/helpers.ts
+++ src/core/helpers.ts
@@ -512,6 +512,7 @@ function printPaddedMessage(message: string, totalWidth: number): string {
  * @param version The version of the chart
  * @param type The action that was performed such as 'Installed' or 'Upgraded'
  */
+// TODO convert usages to leverage the logger.addMessageGroupMessage()
 export function showVersionBanner(logger: SoloLogger, chartName: string, version: string, type: string = 'Installed') {
   logger.showUser(chalk.cyan(` - ${type} ${chartName} chart, version:`, chalk.yellow(version)));
 }
