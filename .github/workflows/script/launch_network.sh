a069a99ac289f578e5128fa82881fecc402d44f4
Zhan Milenkov <instamenta@abv.bg>
2025-07-15 16:27:48 +0300

    attempt to fix migration test
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -32,21 +32,21 @@ echo "Launch solo using released Solo version ${releaseTag}"
 
 
 solo init
-solo cluster-ref config setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
-solo consensus node keys --gossip-keys --tls-keys -i node1,node2
-solo deployment config create -i node1,node2 -n "${SOLO_NAMESPACE}" --context kind-"${SOLO_CLUSTER_NAME}" --email john@doe.com --deployment-clusters kind-"${SOLO_CLUSTER_NAME}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --deployment "${SOLO_DEPLOYMENT}"
+solo cluster setup -s "${SOLO_CLUSTER_SETUP_NAMESPACE}"
+solo node keys --gossip-keys --tls-keys -i node1,node2
+solo deployment create -i node1,node2 -n "${SOLO_NAMESPACE}" --context kind-"${SOLO_CLUSTER_NAME}" --email john@doe.com --deployment-clusters kind-"${SOLO_CLUSTER_NAME}" --cluster-ref kind-${SOLO_CLUSTER_NAME} --deployment "${SOLO_DEPLOYMENT}"
 
 export CONSENSUS_NODE_VERSION=v0.58.10
 # Use custom settings file for the deployment to avoid too many state saved in disk causing the no space left on device error
-solo consensus network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q --settings-txt .github/workflows/support/v58-test/settings.txt
-solo consensus node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
-solo consensus node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
-solo ledger account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
+solo network deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --pvcs --release-tag "${CONSENSUS_NODE_VERSION}" -q --settings-txt .github/workflows/support/v58-test/settings.txt
+solo node setup -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --release-tag "${CONSENSUS_NODE_VERSION}" -q
+solo node start -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" -q
+solo account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
 
 
-solo mirror node deploy  --deployment "${SOLO_DEPLOYMENT}" --pinger
-solo explorer node deploy -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
-solo relay node deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
+solo mirror-node deploy  --deployment "${SOLO_DEPLOYMENT}" --pinger
+solo explorer deploy -s "${SOLO_CLUSTER_SETUP_NAMESPACE}" --deployment "${SOLO_DEPLOYMENT}" --cluster-ref kind-${SOLO_CLUSTER_NAME}
+solo relay deploy -i node1,node2 --deployment "${SOLO_DEPLOYMENT}"
 
 cp ~/.solo/cache/local-config.yaml ./local-config-before.yaml
 cat ./local-config-before.yaml
@@ -55,7 +55,7 @@ cat remote-config-before.yaml
 
 # must uninstall explorer before migration, because the change of explorer chart name and labels
 # make it harder to uninstall or upgrade after migration
- solo explorer node destroy --deployment "${SOLO_DEPLOYMENT}" --force
+solo explorer destroy --deployment "${SOLO_DEPLOYMENT}" --force
 
 # trigger migration
 npm run solo-test -- ledger account create --deployment "${SOLO_DEPLOYMENT}"
