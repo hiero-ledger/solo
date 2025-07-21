d53a223df42734e50cb41fb8ce9d218f352a928a
Jeromy Cannon <jeromy@swirldslabs.com>
2025-07-21 22:15:55 +0100

    chore: update consensus node version to v0.61.7 and adjust upgrade test version to v0.62.10
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- version-test.ts
+++ version-test.ts
@@ -2,6 +2,5 @@
 // using a different version than the one in version.ts to test backwards compatibility
 
 export const TEST_LOCAL_HEDERA_PLATFORM_VERSION: string = 'v0.61.7';
-// export const TEST_UPGRADE_VERSION: string = 'v0.63.9';
-export const TEST_UPGRADE_VERSION: string = 'v0.62.6';
+export const TEST_UPGRADE_VERSION: string = 'v0.62.10';
 export const TEST_LOCAL_BLOCK_NODE_VERSION: string = '0.12.0';
