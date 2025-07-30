a4d4e36e53b97ba7542176b920e0d370708e82c1
Jan Milenkov <instamenta@abv.bg>
2025-07-30 23:30:12 +0300

    fix: remove logs of 'argv' and 'context.config' (#2378)
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- src/commands/relay.ts
+++ src/commands/relay.ts
@@ -321,8 +321,6 @@ export class RelayCommand extends BaseCommand {
               }
             }
 
-            self.logger.debug('Initialized config', {config: context_.config});
-
             return ListrLock.newAcquireLockTask(lease, task);
           },
         },
@@ -531,8 +529,6 @@ export class RelayCommand extends BaseCommand {
               context_.config.context,
             );
 
-            self.logger.debug('Initialized config', {config: context_.config});
-
             return ListrLock.newAcquireLockTask(lease, task);
           },
         },
@@ -584,7 +580,7 @@ export class RelayCommand extends BaseCommand {
               flags.setOptionalCommandFlags(y, ...RelayCommand.DEPLOY_FLAGS_LIST.optional);
             },
             handler: async (argv: ArgvStruct) => {
-              self.logger.info("==== Running 'relay deploy' ===", {argv});
+              self.logger.info("==== Running 'relay deploy' ===");
 
               await self.deploy(argv).then(r => {
                 self.logger.info('==== Finished running `relay deploy`====');
@@ -602,8 +598,7 @@ export class RelayCommand extends BaseCommand {
               flags.setOptionalCommandFlags(y, ...RelayCommand.DESTROY_FLAGS_LIST.optional);
             },
             handler: async (argv: ArgvStruct) => {
-              self.logger.info("==== Running 'relay destroy' ===", {argv});
-              self.logger.debug(argv);
+              self.logger.info("==== Running 'relay destroy' ===");
 
               await self.destroy(argv).then(r => {
                 self.logger.info('==== Finished running `relay destroy`====');
