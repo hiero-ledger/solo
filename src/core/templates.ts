9288a341a6c91c3e144aa0f896112990c5e5665d
Zhan Milenkov <instamenta@abv.bg>
2025-07-31 16:38:57 +0300

    fix label creation for validating relay
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- src/core/templates.ts
+++ src/core/templates.ts
@@ -337,11 +337,9 @@ export class Templates {
   // Component Label Selectors
 
   public static renderRelayLabels(id: ComponentId, legacyReleaseName?: string): string[] {
-    const releaseName: string = legacyReleaseName
-      ? constants.JSON_RPC_RELAY_CHART
-      : `${constants.JSON_RPC_RELAY_RELEASE_NAME}-${id}`;
-
-    return [`app.kubernetes.io/instance=${releaseName}`];
+    return legacyReleaseName
+      ? ['app.kubernetes.io/name=relay']
+      : [`app.kubernetes.io/instance=${constants.JSON_RPC_RELAY_RELEASE_NAME}-${id}`];
   }
 
   public static renderHaProxyLabels(id: ComponentId): string[] {
