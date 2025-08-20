679d89e4ae2928c1f0446eefbec438fdb750e0da
Jeromy Cannon <jeromy@swirldslabs.com>
2025-08-20 18:58:40 +0100

    fix: update TEST_UPGRADE_VERSION to v0.64.2 for compatibility testing
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- version-test.ts
+++ version-test.ts
@@ -2,5 +2,5 @@
 // using a different version than the one in version.ts to test backwards compatibility
 
 export const TEST_LOCAL_HEDERA_PLATFORM_VERSION: string = 'v0.63.9';
-export const TEST_UPGRADE_VERSION: string = 'v0.64.3';
+export const TEST_UPGRADE_VERSION: string = 'v0.64.2';
 export const TEST_LOCAL_BLOCK_NODE_VERSION: string = '0.15.0';
