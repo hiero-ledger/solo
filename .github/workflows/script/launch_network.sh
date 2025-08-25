b19c83c5f74b93e960d3157839bce67660c78bef
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-08-25 13:45:37 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -105,7 +105,7 @@ echo "::group::Upgrade Consensus Node"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Check existing port-forward before upgrade consensus node"
 ps -ef |grep port-forward
 # Upgrade to latest version
-export CONSENSUS_NODE_VERSION=$(grep 'HEDERA_PLATFORM_VERSION' version.ts | sed -E "s/.*'([^']+)';/\1/")
+export CONSENSUS_NODE_VERSION=$(tr -d '\n' < "version.ts" | grep -oE "HEDERA_PLATFORM_VERSION.*?'(v[0-9.]+(-rc[0-9]+)?)'" | grep -oE "v[0-9.]+(-rc[0-9]+)?")
 npm run solo -- consensus network upgrade -i node1,node2 --deployment "${SOLO_DEPLOYMENT}" --upgrade-version "${CONSENSUS_NODE_VERSION}" -q
 npm run solo -- ledger account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
 echo "::endgroup::"
