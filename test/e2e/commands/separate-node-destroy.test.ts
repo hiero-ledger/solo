c59d07aafdfce18c61c898c55d175a20e9a378a2
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-09-02 19:54:39 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- test/e2e/commands/separate-node-destroy.test.ts
+++ test/e2e/commands/separate-node-destroy.test.ts
@@ -10,12 +10,9 @@ import {
   endToEndTestSuite,
   HEDERA_PLATFORM_VERSION_TAG,
 } from '../../test-utility.js';
-import {HEDERA_HAPI_PATH, ROOT_CONTAINER} from '../../../src/core/constants.js';
 import {type NodeAlias} from '../../../src/types/aliases.js';
 import {Duration} from '../../../src/core/time/duration.js';
 import {NamespaceName} from '../../../src/types/namespace/namespace-name.js';
-import {PodReference} from '../../../src/integration/kube/resources/pod/pod-reference.js';
-import {ContainerReference} from '../../../src/integration/kube/resources/container/container-reference.js';
 import {type NetworkNodes} from '../../../src/core/network-nodes.js';
 import {container} from 'tsyringe-neo';
 import {InjectTokens} from '../../../src/core/dependency-injection/inject-tokens.js';
@@ -102,13 +99,7 @@ endToEndTestSuite(namespace.name, argv, {}, bootstrapResp => {
     it('deleted consensus node should not be running', async () => {
       // read config.txt file from first node, read config.txt line by line, it should not contain value of nodeAlias
       const pods: Pod[] = await k8Factory.default().pods().list(namespace, ['solo.hedera.com/type=network-node']);
-      const response = await k8Factory
-        .default()
-        .containers()
-        .readByRef(ContainerReference.of(PodReference.of(namespace, pods[0].podReference.name), ROOT_CONTAINER))
-        .execContainer(['bash', '-c', `tail -n 1 ${HEDERA_HAPI_PATH}/output/swirlds.log`]);
-
-      expect(response).to.contain('JVM is shutting down');
+      expect(pods.length).to.equal(0);
     }).timeout(Duration.ofMinutes(10).toMillis());
   });
 });
