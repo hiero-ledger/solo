dc34b719fff6f56adf77c9155e446ce55f458d17
Jeromy Cannon <jeromy@swirldslabs.com>
2025-09-25 17:52:10 +0100

    feat: streamline metrics-server deployment in dual E2E setup
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/integration/kube/kube-validation.ts
+++ src/integration/kube/kube-validation.ts
@@ -5,6 +5,23 @@
  * @param value - the string to check
  * @returns true if the string is a valid DNS-1123 label
  */
-export function isDns1123Label(value: string) {
+export function isDns1123Label(value: string): boolean {
   return /^[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?$/.test(value);
+}
+
+/**
+ * @include DNS_1123_OBJECT
+ * @param value - the string to check
+ * @returns true if the string is a valid DNS-1123 object
+ */
+export function isDns1123Object(value: string): boolean {
+  return /^[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?$/.test(value);
+}
+/**
+ * @include DNS_1123_RESOURCE
+ * @param value - the string to check
+ * @returns true if the string is a valid DNS-1123 resource
+ */
+export function isDns1123Resource(value: string): boolean {
+  return isDns1123Object(value);
 }
