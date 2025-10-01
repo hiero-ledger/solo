0fb4e215d0bfce0ae8e2011ee41bd3e6f706afeb
Jan Milenkov <instamenta@abv.bg>
2025-10-01 16:17:52 +0300

    fix(cli): consolidate node add/update/delete 'separate-' commands into 'node-add-local' and fix the underlying cli commands (#2687)
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- test/e2e/commands/node-add-local.test.ts
+++ test/e2e/commands/node-add-local.test.ts
@@ -1,135 +1,73 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import {describe, it} from 'mocha';
-import {expect} from 'chai';
+import {after, describe} from 'mocha';
 
-import {testNodeAdd} from '../../test-add.js';
 import {Duration} from '../../../src/core/time/duration.js';
-import {type BootstrapResponse} from '../../test-utility.js';
-import {Flags as flags} from '../../../src/commands/flags.js';
-import {
-  type AccountBalance,
-  AccountBalanceQuery,
-  AccountCreateTransaction,
-  Hbar,
-  HbarUnit,
-  PrivateKey,
-  type TransactionReceipt,
-  type TransactionResponse,
-} from '@hiero-ledger/sdk';
-import {type DeploymentName} from '../../../src/types/index.js';
+import {testSeparateNodeAdd} from './separate-node-add.test.js';
 import {NamespaceName} from '../../../src/types/namespace/namespace-name.js';
-import {type Argv} from '../../helpers/argv-wrapper.js';
-import {ConsensusCommandDefinition} from '../../../src/commands/command-definitions/consensus-command-definition.js';
-import {PathEx} from '../../../src/business/utils/path-ex.js';
-import {SOLO_LOGS_DIR} from '../../../src/core/constants.js';
-import {LedgerCommandDefinition} from '../../../src/commands/command-definitions/ledger-command-definition.js';
-import {sleep} from '../../../src/core/helpers.js';
-import {type NetworkNodes} from '../../../src/core/network-nodes.js';
+import {Argv} from '../../helpers/argv-wrapper.js';
+import {Flags as flags} from '../../../src/commands/flags.js';
+import {endToEndTestSuite, getTestCluster} from '../../test-utility.js';
+import {TEST_LOCAL_HEDERA_PLATFORM_VERSION} from '../../../version-test.js';
 import {container} from 'tsyringe-neo';
+import {type NetworkNodes} from '../../../src/core/network-nodes.js';
 import {InjectTokens} from '../../../src/core/dependency-injection/inject-tokens.js';
+import {ConsensusCommandDefinition} from '../../../src/commands/command-definitions/consensus-command-definition.js';
+import {testSeparateNodeUpdate} from './separate-node-update.test.js';
+import {testSeparateNodeDelete} from './separate-node-destroy.test.js';
+import {ADD_EXECUTE_FLAGS} from '../../../src/commands/node/flags.js';
 
-function additionalTests(bootstrapResp: BootstrapResponse, argv: Argv): void {
-  const {
-    opts: {commandInvoker, remoteConfig},
-    cmd: {nodeCmd, accountCmd},
-    manager: {accountManager},
-  } = bootstrapResp;
-
-  const namespace: NamespaceName = NamespaceName.of(argv.getArg(flags.namespace));
-
-  it('should save the state, restart node, and preserve account balances', async (): Promise<void> => {
-    // create account before stopping
-    await accountManager.loadNodeClient(
-      namespace,
-      remoteConfig.getClusterRefs(),
-      argv.getArg<DeploymentName>(flags.deployment),
-      argv.getArg<boolean>(flags.forcePortForward),
-    );
-
-    const privateKey: PrivateKey = PrivateKey.generate();
-    // get random integer between 100 and 1000
-    const amount: number = Math.floor(Math.random() * (1000 - 100) + 100);
-
-    const newAccount: TransactionResponse = await new AccountCreateTransaction()
-      .setKeyWithoutAlias(privateKey.publicKey)
-      .setInitialBalance(Hbar.from(amount, HbarUnit.Hbar))
-      .execute(accountManager._nodeClient);
-
-    // Get the new account ID
-    const getReceipt: TransactionReceipt = await newAccount.getReceipt(accountManager._nodeClient);
-    const accountInfo: {accountId: string; balance: number} = {
-      accountId: getReceipt.accountId.toString(),
-      balance: amount,
-    };
-
-    // create more transactions to save more round of states
-    await commandInvoker.invoke({
-      argv: argv,
-      command: LedgerCommandDefinition.COMMAND_NAME,
-      subcommand: LedgerCommandDefinition.ACCOUNT_SUBCOMMAND_NAME,
-      action: LedgerCommandDefinition.ACCOUNT_CREATE,
-      callback: async (argv): Promise<boolean> => accountCmd.create(argv),
-    });
-
-    await sleep(Duration.ofSeconds(1));
-
-    await commandInvoker.invoke({
-      argv: argv,
-      command: LedgerCommandDefinition.COMMAND_NAME,
-      subcommand: LedgerCommandDefinition.ACCOUNT_SUBCOMMAND_NAME,
-      action: LedgerCommandDefinition.ACCOUNT_CREATE,
-      callback: async (argv): Promise<boolean> => accountCmd.create(argv),
-    });
-
-    await commandInvoker.invoke({
-      argv: argv,
-      command: ConsensusCommandDefinition.COMMAND_NAME,
-      subcommand: ConsensusCommandDefinition.NETWORK_SUBCOMMAND_NAME,
-      action: ConsensusCommandDefinition.NETWORK_FREEZE,
-      callback: async (argv): Promise<boolean> => nodeCmd.handlers.freeze(argv),
-    });
-
-    await commandInvoker.invoke({
-      argv,
-      command: ConsensusCommandDefinition.COMMAND_NAME,
-      subcommand: ConsensusCommandDefinition.STATE_SUBCOMMAND_NAME,
-      action: ConsensusCommandDefinition.STATE_DOWNLOAD,
-      callback: async (argv): Promise<boolean> => nodeCmd.handlers.states(argv),
-    });
-
-    await commandInvoker.invoke({
-      argv: argv,
-      command: ConsensusCommandDefinition.COMMAND_NAME,
-      subcommand: ConsensusCommandDefinition.NODE_SUBCOMMAND_NAME,
-      action: ConsensusCommandDefinition.NODE_RESTART,
-      callback: async (argv): Promise<boolean> => nodeCmd.handlers.restart(argv),
-    });
-
-    argv.setArg(flags.stateFile, PathEx.joinWithRealPath(SOLO_LOGS_DIR, namespace.name, 'network-node1-0-state.zip'));
-
-    // check balance of accountInfo.accountId
-    await accountManager.loadNodeClient(
-      namespace,
-      remoteConfig.getClusterRefs(),
-      argv.getArg<DeploymentName>(flags.deployment),
-      argv.getArg<boolean>(flags.forcePortForward),
-    );
-
-    const balance: AccountBalance = await new AccountBalanceQuery()
-      .setAccountId(accountInfo.accountId)
-      .execute(accountManager._nodeClient);
-
-    expect(balance.hbars).to.be.eql(Hbar.from(accountInfo.balance, HbarUnit.Hbar));
-  }).timeout(Duration.ofMinutes(10).toMillis());
-
-  it('get the logs', async (): Promise<void> => {
-    await container.resolve<NetworkNodes>(InjectTokens.NetworkNodes).getLogs(namespace);
-  }).timeout(Duration.ofMinutes(10).toMillis());
-}
+console.log(ADD_EXECUTE_FLAGS);
 
 describe('Node add with hedera local build', (): void => {
-  const localBuildPath: string =
-    'node1=../hiero-consensus-node/hedera-node/data/,../hiero-consensus-node/hedera-node/data,node3=../hiero-consensus-node/hedera-node/data';
-  testNodeAdd(localBuildPath, undefined, undefined, additionalTests);
+  const localBuildPath: string = [
+    'node1=../hiero-consensus-node/hedera-node/data/',
+    '../hiero-consensus-node/hedera-node/data',
+    'node3=../hiero-consensus-node/hedera-node/data',
+  ].join(',');
+
+  const suffix: string = localBuildPath.slice(0, 5);
+  const namespace: NamespaceName = NamespaceName.of(`node-add${suffix}`);
+  const argv: Argv = Argv.getDefaultArgv(namespace);
+  argv.setArg(flags.clusterRef, getTestCluster());
+  argv.setArg(flags.nodeAliasesUnparsed, 'node1,node2');
+  argv.setArg(flags.stakeAmounts, '1500,1');
+  argv.setArg(flags.generateGossipKeys, true);
+  argv.setArg(flags.generateTlsKeys, true);
+  argv.setArg(flags.releaseTag, TEST_LOCAL_HEDERA_PLATFORM_VERSION);
+  argv.setArg(flags.namespace, namespace.name);
+  argv.setArg(flags.force, true);
+  argv.setArg(flags.persistentVolumeClaims, true);
+  argv.setArg(flags.localBuildPath, localBuildPath);
+  argv.setArg(flags.forcePortForward, true);
+
+  const timeout: number = Duration.ofMinutes(2).toMillis();
+
+  endToEndTestSuite(namespace.name, argv, {}, (bootstrapResp): void => {
+    const {
+      opts: {k8Factory, commandInvoker, accountManager},
+      cmd: {networkCmd},
+    } = bootstrapResp;
+
+    after(async function (): Promise<void> {
+      this.timeout(Duration.ofMinutes(10).toMillis());
+
+      await container.resolve<NetworkNodes>(InjectTokens.NetworkNodes).getLogs(namespace);
+      await accountManager.close();
+
+      await commandInvoker.invoke({
+        argv: argv,
+        command: ConsensusCommandDefinition.COMMAND_NAME,
+        subcommand: ConsensusCommandDefinition.NETWORK_SUBCOMMAND_NAME,
+        action: ConsensusCommandDefinition.NETWORK_DESTROY,
+        callback: async (argv): Promise<boolean> => networkCmd.destroy(argv),
+      });
+
+      await k8Factory.default().namespaces().delete(namespace);
+    });
+
+    testSeparateNodeAdd(argv.clone(), bootstrapResp, namespace, timeout);
+    testSeparateNodeUpdate(argv.clone(), bootstrapResp, namespace, timeout);
+    testSeparateNodeDelete(argv.clone(), bootstrapResp, namespace);
+  });
 }).timeout(Duration.ofMinutes(3).toMillis());
