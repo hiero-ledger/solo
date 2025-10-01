3eb92790f00f489fef553b2d3fa6457648e62868
Zhan Milenkov <instamenta@abv.bg>
2025-10-01 09:23:17 +0300

    cleanup
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- test/e2e/commands/separate-node-update.test.ts
+++ test/e2e/commands/separate-node-update.test.ts
@@ -62,16 +62,6 @@ export function testSeparateNodeUpdate(
         getTemporaryDirectory(),
       );
     }).timeout(Duration.ofMinutes(8).toMillis());
-    //
-    // it('should succeed with init command', async (): Promise<void> => {
-    //   await commandInvoker.invoke({
-    //     argv: argv,
-    //     command: LedgerCommandDefinition.COMMAND_NAME,
-    //     subcommand: LedgerCommandDefinition.SYSTEM_SUBCOMMAND_NAME,
-    //     action: LedgerCommandDefinition.SYSTEM_INIT,
-    //     callback: async (argv): Promise<boolean> => accountCmd.init(argv),
-    //   });
-    // }).timeout(Duration.ofMinutes(8).toMillis());
 
     it('should update a new node property successfully', async (): Promise<void> => {
       // generate gossip and tls keys for the updated node
