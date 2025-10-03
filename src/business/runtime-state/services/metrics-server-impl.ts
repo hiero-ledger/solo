ba7ed51b2231ab5889cc714f44697b1191b22daf
Jeromy Cannon <jeromy@swirldslabs.com>
2025-10-03 12:38:37 +0100

    fix: enhance cluster metrics with runtime and transaction count tracking
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/business/runtime-state/services/metrics-server-impl.ts
+++ src/business/runtime-state/services/metrics-server-impl.ts
@@ -6,7 +6,7 @@ import {NamespaceName} from '../../../types/namespace/namespace-name.js';
 import {type Context} from '../../../types/index.js';
 import {ShellRunner} from '../../../core/shell-runner.js';
 import {PodName} from '../../../integration/kube/resources/pod/pod-name.js';
-import {injectable} from 'tsyringe-neo';
+import {inject, injectable} from 'tsyringe-neo';
 import {type SoloLogger} from '../../../core/logging/solo-logger.js';
 import {patchInject} from '../../../core/dependency-injection/container-helper.js';
 import {InjectTokens} from '../../../core/dependency-injection/inject-tokens.js';
@@ -14,11 +14,19 @@ import fs from 'node:fs';
 import {AggregatedMetrics} from '../model/aggregated-metrics.js';
 import {ClusterMetrics} from '../model/cluster-metrics.js';
 import yaml from 'yaml';
+import {type K8Factory} from '../../../integration/kube/k8-factory.js';
+import {ContainerReference} from '../../../integration/kube/resources/container/container-reference.js';
+import {ContainerName} from '../../../integration/kube/resources/container/container-name.js';
+import {PodReference} from '../../../integration/kube/resources/pod/pod-reference.js';
 
 @injectable()
 export class MetricsServerImpl implements MetricsServer {
-  public constructor(public logger?: SoloLogger) {
-    this.logger = patchInject<SoloLogger>(logger, InjectTokens.SoloLogger, this.constructor.name);
+  public constructor(
+    @inject(InjectTokens.SoloLogger) private readonly logger?: SoloLogger,
+    @inject(InjectTokens.K8Factory) private readonly k8Factory?: K8Factory,
+  ) {
+    this.logger = patchInject(logger, InjectTokens.SoloLogger, this.constructor.name);
+    this.k8Factory = patchInject(k8Factory, InjectTokens.K8Factory, this.constructor.name);
   }
 
   public async getMetrics(
@@ -62,6 +70,8 @@ export class MetricsServerImpl implements MetricsServer {
       let cpuInMillicores: number;
       let memoryInMebibytes: number;
       let index: number = 0;
+      let clusterNamespace: string = '';
+      let mirrorNodePostgresPodName: string = undefined;
       const resultArray: string[] = joinedResults
         .trim()
         .split(/\r?\n|\n| +/)
@@ -74,8 +84,19 @@ export class MetricsServerImpl implements MetricsServer {
         podMetrics.push(
           new PodMetrics(NamespaceName.of(namespace), PodName.of(podName), cpuInMillicores, memoryInMebibytes),
         );
+        if (podName.startsWith('network-node1-0')) {
+          clusterNamespace = namespace;
+        }
+        if (podName.includes('postgres')) {
+          mirrorNodePostgresPodName = podName;
+        }
       }
-      return this.createClusterMetrics(context ?? 'default', podMetrics);
+      return this.createClusterMetrics(
+        context ?? 'default',
+        NamespaceName.of(clusterNamespace),
+        podMetrics,
+        mirrorNodePostgresPodName ? PodName.of(mirrorNodePostgresPodName) : undefined,
+      );
     } catch (error) {
       if (error.message.includes('Metrics API not available')) {
         this.logger.showUser('Metrics API not available for reporting metrics');
@@ -85,21 +106,45 @@ export class MetricsServerImpl implements MetricsServer {
     }
   }
 
-  private createAggregatedMetrics(snapshotName: string, clusterMetrics: ClusterMetrics[]): AggregatedMetrics {
+  private async createAggregatedMetrics(
+    snapshotName: string,
+    clusterMetrics: ClusterMetrics[],
+  ): Promise<AggregatedMetrics> {
     if (!clusterMetrics || clusterMetrics?.length === 0) {
       return undefined;
     }
 
     let cpuInMillicores: number = 0;
     let memoryInMebibytes: number = 0;
+    let runtime: number = 0;
+    let transactions: number = 0;
     for (const clusterMetric of clusterMetrics) {
       cpuInMillicores += clusterMetric.cpuInMillicores;
       memoryInMebibytes += clusterMetric.memoryInMebibytes;
+      runtime += await this.getNetworkNodeRuntime(clusterMetric.namespace, clusterMetric.context);
+      transactions += await this.getNetworkTransactions(
+        clusterMetric.namespace,
+        clusterMetric.context,
+        clusterMetric.postgresPodName,
+      );
     }
-    return new AggregatedMetrics(snapshotName, clusterMetrics, cpuInMillicores, memoryInMebibytes);
+
+    return new AggregatedMetrics(
+      snapshotName,
+      clusterMetrics,
+      cpuInMillicores,
+      memoryInMebibytes,
+      runtime,
+      transactions,
+    );
   }
 
-  private createClusterMetrics(context: Context, podMetrics: PodMetrics[]): ClusterMetrics {
+  private createClusterMetrics(
+    context: Context,
+    namespace: NamespaceName,
+    podMetrics: PodMetrics[],
+    mirrorNodePostgresPodName: PodName,
+  ): ClusterMetrics {
     if (!podMetrics || podMetrics?.length === 0) {
       return undefined;
     }
@@ -110,7 +155,14 @@ export class MetricsServerImpl implements MetricsServer {
       cpuInMillicores += podMetric.cpuInMillicores;
       memoryInMebibytes += podMetric.memoryInMebibytes;
     }
-    return new ClusterMetrics(context, podMetrics, cpuInMillicores, memoryInMebibytes);
+    return new ClusterMetrics(
+      context,
+      namespace,
+      podMetrics,
+      mirrorNodePostgresPodName,
+      cpuInMillicores,
+      memoryInMebibytes,
+    );
   }
 
   public async logMetrics(
@@ -129,5 +181,37 @@ export class MetricsServerImpl implements MetricsServer {
 
     fs.writeFileSync(`${metricsLogFile}.json`, aggregatedMetrics ? aggregatedMetrics.toString() : '');
     fs.writeFileSync(`${metricsLogFile}.yaml`, aggregatedMetrics ? yaml.stringify(aggregatedMetrics) : '');
+  }
+
+  private async getNetworkNodeRuntime(namespace: NamespaceName, context: Context): Promise<number> {
+    const contextParameter: string = context && context !== 'default' ? `--context ${context}` : '';
+    const cmd: string = `kubectl get pod network-node1-0 -n ${namespace.name} --no-headers ${contextParameter} | awk '{print $5}'`;
+    const results: string[] = await new ShellRunner().run(cmd, [], true, false);
+    if (results?.length > 0) {
+      return Number.parseInt(results[0].split('m')[0]);
+    }
+    return 0;
+  }
+
+  private async getNetworkTransactions(
+    namespace: NamespaceName,
+    context: Context,
+    postgresPodName: PodName,
+  ): Promise<number> {
+    try {
+      const result: string = await this.k8Factory
+        .getK8(context && context !== 'default' ? context : undefined)
+        .containers()
+        .readByRef(ContainerReference.of(PodReference.of(namespace, postgresPodName), ContainerName.of('postgresql')))
+        .execContainer([
+          'bash',
+          '-c',
+          "PGPASSWORD=$(cat $POSTGRES_PASSWORD_FILE) psql -U postgres -d mirror_node -c 'select count(*) from transaction;' -t",
+        ]);
+      return Number.parseInt(result.trim());
+    } catch (error) {
+      this.logger.debug(`error looking up transactions: ${error.message}`, error);
+    }
+    return 0;
   }
 }
