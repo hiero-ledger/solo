2ec1edec6cad35c7ed19edf8418ead246da80386
Tom <tomsdysons@gmail.com>
2025-10-13 20:16:12 +0200

    chore: fix typos (#2650)
    Signed-off-by: tomsdy <tomsdysons@gmail.com>

--- src/commands/flags.ts
+++ src/commands/flags.ts
@@ -2064,7 +2064,7 @@ export class Flags {
       describe:
         'TLS Certificate key path for the gRPC ' +
         '(e.g. "node1=/Users/username/node1-grpc.key" ' +
-        'with multiple nodes comma seperated)',
+        'with multiple nodes comma separated)',
       defaultValue: '',
       type: 'string',
       dataMask: constants.STANDARD_DATAMASK,
@@ -2091,7 +2091,7 @@ export class Flags {
       describe:
         'TLC Certificate key path for gRPC Web ' +
         '(e.g. "node1=/Users/username/node1-grpc-web.key" ' +
-        'with multiple nodes comma seperated)',
+        'with multiple nodes comma separated)',
       defaultValue: '',
       type: 'string',
       dataMask: constants.STANDARD_DATAMASK,
@@ -2116,7 +2116,7 @@ export class Flags {
     name: 'stake-amounts',
     definition: {
       describe:
-        'The amount to be staked in the same order you list the node aliases with multiple node staked values comma seperated',
+        'The amount to be staked in the same order you list the node aliases with multiple node staked values comma separated',
       defaultValue: '',
       type: 'string',
     },
@@ -2523,7 +2523,7 @@ export class Flags {
       describe:
         'Custom domain names for consensus nodes mapping for the' +
         `${chalk.gray('(e.g. node0=domain.name where key is node alias and value is domain name)')}` +
-        'with multiple nodes comma seperated',
+        'with multiple nodes comma separated',
       type: 'string',
     },
     prompt: undefined,
