0221643b504bb6b593344e4c40487c8ab7f1b22d
Zhan Milenkov <instamenta@abv.bg>
2025-11-04 11:49:35 +0200

    increase timeout and add debug log
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- src/commands/network.ts
+++ src/commands/network.ts
@@ -1403,6 +1403,30 @@ export class NetworkCommand extends BaseCommand {
           },
         },
         {
+          title: 'test TODO: remove',
+          task: async ({config: {contexts, namespace, deletePvcs, deleteSecrets, deployment}}): Promise<void> => {
+            for (const context of contexts) {
+              const secrets: string[] = await this.k8Factory
+                .getK8(context)
+                .secrets()
+                .list(namespace)
+                .then(data => data.map(secret => secret.name));
+
+              const pvcs: string[] = await this.k8Factory.getK8(context).pvcs().list(namespace, []);
+
+              console.log({
+                secrets,
+                pvcs,
+                namespace,
+                contexts,
+                deletePvcs,
+                deleteSecrets,
+                deployment,
+              });
+            }
+          },
+        },
+        {
           title: 'Running sub-tasks to destroy network',
           task: async (
             {config: {enableTimeout, deletePvcs, deleteSecrets, namespace, contexts}},
@@ -1432,30 +1456,6 @@ export class NetworkCommand extends BaseCommand {
             await this.destroyTask(task, namespace, deletePvcs, deleteSecrets, contexts);
 
             clearTimeout(onTimeoutCallback);
-          },
-        },
-        {
-          title: 'test TODO: remove',
-          task: async ({config: {contexts, namespace, deletePvcs, deleteSecrets, deployment}}): Promise<void> => {
-            for (const context of contexts) {
-              const secrets: string[] = await this.k8Factory
-                .getK8(context)
-                .secrets()
-                .list(namespace)
-                .then(data => data.map(secret => secret.name));
-
-              const pvcs: string[] = await this.k8Factory.getK8(context).pvcs().list(namespace, []);
-
-              console.log({
-                secrets,
-                pvcs,
-                namespace,
-                contexts,
-                deletePvcs,
-                deleteSecrets,
-                deployment,
-              });
-            }
           },
         },
       ],
