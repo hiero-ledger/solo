50a6612e49acfafc0ebec9d2c87e141547112fb0
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-01-04 22:29:47 -0600

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- src/commands/backup-restore.ts
+++ src/commands/backup-restore.ts
@@ -488,7 +488,7 @@ export class BackupRestoreCommand extends BaseCommand {
 
         // Fix ownership of extracted files to hedera user
         this.logger.showUser(chalk.gray(`    Setting ownership for extracted files in pod: ${podName}`));
-        await container.execContainer(['bash', '-c', `sudo chown -R hedera:hedera ${constants.HEDERA_HAPI_PATH}`]);
+        await container.execContainer(['bash', '-c', `chown -R hedera:hedera ${constants.HEDERA_HAPI_PATH}`]);
 
         this.logger.showUser(chalk.green(`    ✓ Restored log for pod: ${podName}`));
       }
