bf1e118883b3c955078fd8b93a93fc48ee2dfc29
Jeromy Cannon <jeromy@swirldslabs.com>
2026-01-16 19:22:22 +0000

    refactor(rbac): enhance error handling and response validation in K8ClientRbacs methods
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/integration/kube/k8-client/resources/rbac/k8-client-rbacs.ts
+++ src/integration/kube/k8-client/resources/rbac/k8-client-rbacs.ts
@@ -2,8 +2,13 @@
 
 import {type Rbacs} from '../../../resources/rbac/rbacs.js';
 import {type ClusterRole} from '../../../resources/rbac/cluster-role.js';
-import {type RbacAuthorizationV1Api} from '@kubernetes/client-node';
+import {type V1ClusterRole, type RbacAuthorizationV1Api, type V1Status} from '@kubernetes/client-node';
 import {K8ClientClusterRole} from './k8-client-cluster-role.js';
+import {type IncomingMessage} from 'node:http';
+import {ResourceCreateError, ResourceDeleteError} from '../../../errors/resource-operation-errors.js';
+import {ResourceType} from '../../../resources/resource-type.js';
+import {KubeApiResponse} from '../../../kube-api-response.js';
+import {ResourceOperation} from '../../../resources/resource-operation.js';
 
 export class K8ClientRbacs implements Rbacs {
   public constructor(private readonly k8sRbacApi: RbacAuthorizationV1Api) {}
@@ -18,12 +23,26 @@ export class K8ClientRbacs implements Rbacs {
     labels?: Record<string, string>,
   ): Promise<void> {
     const clusterRole: ClusterRole = new K8ClientClusterRole(name, rules, labels);
-    await this.k8sRbacApi.createClusterRole(clusterRole.toV1ClusterRole());
+    let result: {response: IncomingMessage; body: V1ClusterRole};
+    try {
+      result = await this.k8sRbacApi.createClusterRole(clusterRole.toV1ClusterRole());
+    } catch (error) {
+      throw new ResourceCreateError(ResourceType.RBAC, undefined, name, error);
+    }
+
+    KubeApiResponse.check(result.response, ResourceOperation.CREATE, ResourceType.RBAC, undefined, name);
   }
 
   public async clusterRoleExists(name: string): Promise<boolean> {
+    let result: {response: IncomingMessage; body: V1ClusterRole};
     try {
-      await this.k8sRbacApi.readClusterRole(name);
+      result = await this.k8sRbacApi.readClusterRole(name);
+    } catch {
+      return false;
+    }
+
+    try {
+      KubeApiResponse.check(result.response, ResourceOperation.READ, ResourceType.RBAC, undefined, name);
     } catch {
       return false;
     }
@@ -32,6 +51,13 @@ export class K8ClientRbacs implements Rbacs {
   }
 
   public async deleteClusterRole(name: string): Promise<void> {
-    await this.k8sRbacApi.deleteClusterRole(name);
+    let result: {response: IncomingMessage; body: V1Status};
+    try {
+      result = await this.k8sRbacApi.deleteClusterRole(name);
+    } catch (error) {
+      throw new ResourceDeleteError(ResourceType.RBAC, undefined, name, error);
+    }
+
+    KubeApiResponse.check(result.response, ResourceOperation.DELETE, ResourceType.RBAC, undefined, name);
   }
 }
