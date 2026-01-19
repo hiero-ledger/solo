45bda25c673b773ce066e864c95a12000326fd98
Zhan Milenkov <instamenta@abv.bg>
2026-01-19 13:32:58 +0200

    added conditional logic for supporting both legacy and new schema
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- src/core/block-nodes-json-wrapper.ts
+++ src/core/block-nodes-json-wrapper.ts
@@ -12,11 +12,17 @@ import {InjectTokens} from './dependency-injection/inject-tokens.js';
 import {patchInject} from './dependency-injection/container-helper.js';
 import {type RemoteConfigRuntimeStateApi} from '../business/runtime-state/api/remote-config-runtime-state-api.js';
 
-interface BlockNodeConnectionData {
-  address: string;
-  port: number;
-  priority: number;
-}
+type BlockNodeConnectionData =
+  | {
+      address: string;
+      port: number;
+      priority: number;
+    }
+  | {
+      address: string;
+      streamingPort: number;
+      priority: number;
+    };
 
 interface BlockNodesJsonStructure {
   nodes: BlockNodeConnectionData[];
@@ -35,32 +41,41 @@ export class BlockNodesJsonWrapper implements ToJSON {
   }
 
   public toJSON(): string {
-    const blockNodeConnectionData: BlockNodeConnectionData[] = this.blockNodeMap.map(
-      ([id, priority]): BlockNodeConnectionData => {
-        const blockNodeComponent: BlockNodeStateSchema = this.blockNodeComponents.find(
-          (component): boolean => component.metadata.id === id,
-        );
+    const blockNodeConnectionData: BlockNodeConnectionData[] = [];
 
-        const cluster: ClusterSchema = this.remoteConfig.configuration.clusters.find(
-          (cluster): boolean => cluster.name === blockNodeComponent.metadata.cluster,
-        );
+    for (const [id, priority] of this.blockNodeMap) {
+      const blockNodeComponent: BlockNodeStateSchema = this.blockNodeComponents.find(
+        (component): boolean => component.metadata.id === id,
+      );
 
-        const address: string = Templates.renderSvcFullyQualifiedDomainName(
-          Templates.renderBlockNodeName(blockNodeComponent.metadata.id),
-          blockNodeComponent.metadata.namespace,
-          cluster.dnsBaseDomain,
-        );
+      const cluster: ClusterSchema = this.remoteConfig.configuration.clusters.find(
+        (cluster): boolean => cluster.name === blockNodeComponent.metadata.cluster,
+      );
 
-        const useLegacyPort: boolean = lt(
-          this.remoteConfig.configuration.versions.blockNodeChart,
-          versions.MINIMUM_HIERO_BLOCK_NODE_VERSION_FOR_NEW_LIVENESS_CHECK_PORT,
-        );
+      const address: string = Templates.renderSvcFullyQualifiedDomainName(
+        Templates.renderBlockNodeName(blockNodeComponent.metadata.id),
+        blockNodeComponent.metadata.namespace,
+        cluster.dnsBaseDomain,
+      );
 
-        const port: number = useLegacyPort ? constants.BLOCK_NODE_PORT_LEGACY : constants.BLOCK_NODE_PORT;
+      // Figure out the block node port
+      const useLegacyPort: boolean = lt(
+        this.remoteConfig.configuration.versions.blockNodeChart,
+        versions.MINIMUM_HIERO_BLOCK_NODE_VERSION_FOR_NEW_LIVENESS_CHECK_PORT,
+      );
 
-        return {address, port, priority};
-      },
-    );
+      const port: number = useLegacyPort ? constants.BLOCK_NODE_PORT_LEGACY : constants.BLOCK_NODE_PORT;
+
+      // Figure out field name for port
+      const useLegacyPortName: boolean = lt(
+        this.remoteConfig.configuration.versions.consensusNode,
+        versions.MINIMUM_HIERO_CONSENSUS_NODE_VERSION_FOR_LEGACY_PORT_NAME_FOR_BLOCK_NODES_JSON_FILE,
+      );
+
+      blockNodeConnectionData.push(
+        useLegacyPortName ? {address, port, priority} : {address, streamingPort: port, priority},
+      );
+    }
 
     const data: BlockNodesJsonStructure = {
       nodes: blockNodeConnectionData,
