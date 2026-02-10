8521beb262b545692b19092b803625e0776a945b
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-02-10 13:12:48 -0600

    chore: bump cn version
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version-test.ts
+++ version-test.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // using a different version than the one in version.ts to test backwards compatibility
 
-export const TEST_LOCAL_HEDERA_PLATFORM_VERSION: string = 'v0.67.2';
-export const TEST_UPGRADE_VERSION: string = 'v0.68.7-rc.1';
+export const TEST_LOCAL_HEDERA_PLATFORM_VERSION: string = 'v0.68.7-rc.1';
+export const TEST_UPGRADE_VERSION: string = 'v0.69.0';
 export const TEST_LOCAL_BLOCK_NODE_VERSION: string = '0.22.1';
