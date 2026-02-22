9b17008e0cbed5f6aeb0e7c3c419dd0ab2ee609e
Jeromy Cannon <jeromy@swirldslabs.com>
2026-02-22 10:27:52 +0000

    refactor: improve error handling in deleteClusterRole by utilizing KubeApiResponse
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/integration/kube/k8-client/resources/rbac/k8-client-rbacs.ts
+++ src/integration/kube/k8-client/resources/rbac/k8-client-rbacs.ts
@@ -2,9 +2,8 @@
 
 import {type Rbacs} from '../../../resources/rbac/rbacs.js';
 import {type ClusterRole} from '../../../resources/rbac/cluster-role.js';
-import {type RbacAuthorizationV1Api, type V1Status} from '@kubernetes/client-node';
+import {type RbacAuthorizationV1Api} from '@kubernetes/client-node';
 import {K8ClientClusterRole} from './k8-client-cluster-role.js';
-import {ResourceDeleteError} from '../../../errors/resource-operation-errors.js';
 import {ResourceType} from '../../../resources/resource-type.js';
 import {KubeApiResponse} from '../../../kube-api-response.js';
 import {ResourceOperation} from '../../../resources/resource-operation.js';
@@ -43,13 +42,10 @@ export class K8ClientRbacs implements Rbacs {
   }
 
   public async deleteClusterRole(name: string): Promise<void> {
-    let result: V1Status;
     try {
-      result = await this.k8sRbacApi.deleteClusterRole({name});
+      await this.k8sRbacApi.deleteClusterRole({name});
     } catch (error) {
-      throw new ResourceDeleteError(ResourceType.RBAC, undefined, name, error);
+      KubeApiResponse.throwError(error, ResourceOperation.DELETE, ResourceType.RBAC, undefined, name);
     }
-
-    // KubeApiResponse.check(result.response, ResourceOperation.DELETE, ResourceType.RBAC, undefined, name);
   }
 }
