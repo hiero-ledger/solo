4d013846dcb81850fb31ee3f2ac28f8275cc2d9e
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-04-13 16:59:20 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version-test.ts
+++ version-test.ts
@@ -2,3 +2,4 @@
 // using a different version than the one in version.ts to test backwards compatibility
 
 export const TEST_UPGRADE_FROM_VERSION: string = 'v0.72.0';
+export const TEST_UPGRADDE_TO_VERSION: string = 'v0.73.0-rc.4';
