328b4234533656f4717b8568432140302e5737d7
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-04-17 13:30:55 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- .github/workflows/script/launch_network.sh
+++ .github/workflows/script/launch_network.sh
@@ -204,7 +204,7 @@ curl http://127.0.0.1:37546 || true
 # find the new pod name then enable port-forwarding to it, do not match anything with "ws" in the name
 relayPodName=$(kubectl get pods -n solo-e2e  | grep relay | awk '{print $1}' | grep -v ws)
 echo "Relay Pod Name: ${relayPodName}"
-kubectl port-forward -n solo-e2e --context kind-solo-e2e pods/"${relayPodName}" 37546:7546 &
+kubectl port-forward -n solo-e2e --context kind-solo-e2e pods/"${relayPodName}" 37546:7546 >/tmp/solo-migration-relay-port-forward.log 2>&1 &
 echo "command is kubectl port-forward -n solo-e2e pods/${relayPodName} 37546:7546 &"
 
 # kill existing port-forward process due to restart of mirror ingress controller
@@ -212,7 +212,7 @@ curl http://127.0.0.1:38081 || true
 # find the new mirror-ingress-controller pod name then enable port-forwarding to it
 mirrorPodName=$(kubectl get pods -n solo-e2e  | grep mirror-ingress-controller | awk '{print $1}')
 echo "Mirror Ingress Controller Pod Name: ${mirrorPodName}"
-kubectl port-forward -n solo-e2e --context kind-solo-e2e pods/"${mirrorPodName}" 38081:80 &
+kubectl port-forward -n solo-e2e --context kind-solo-e2e pods/"${mirrorPodName}" 38081:80 >/tmp/solo-migration-mirror-port-forward.log 2>&1 &
 
 # Test transaction can still be sent and processed
 npm run solo -- ledger account create --deployment "${SOLO_DEPLOYMENT}" --hbar-amount 100
