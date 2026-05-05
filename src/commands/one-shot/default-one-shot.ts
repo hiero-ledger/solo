cc4b998cf22e48541311fdf07b22a053f91feca9
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-05-04 23:10:24 -0500

    ci: performance run with edge version
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- src/commands/one-shot/default-one-shot.ts
+++ src/commands/one-shot/default-one-shot.ts
@@ -115,6 +115,7 @@ export class DefaultOneShotCommand extends BaseCommand implements OneShotCommand
       flags.rollback,
       flags.parallelDeploy,
       flags.externalAddress,
+      flags.edgeEnabled,
     ],
   };
 
