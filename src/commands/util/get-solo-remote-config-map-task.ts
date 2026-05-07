eb9ad6148a02610dc665199ddeb491961c78eef9
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-05-07 08:23:58 -0500

    PR comment
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- src/commands/util/get-solo-remote-config-map-task.ts
+++ src/commands/util/get-solo-remote-config-map-task.ts
@@ -5,16 +5,8 @@ import {type SoloListrTask} from '../../types/index.js';
 import {type SoloLogger} from '../../core/logging/solo-logger.js';
 import {type K8Factory} from '../../integration/kube/k8-factory.js';
 import {RemoteConfigCollector} from './remote-config-collector.js';
-import {injectable, inject} from 'tsyringe-neo';
-import {InjectTokens} from '../../core/dependency-injection/inject-tokens.js';
 
-@injectable()
 export class GetSoloRemoteConfigMapTask {
-  public constructor(
-    @inject(InjectTokens.K8Factory) private readonly k8Factory: K8Factory,
-    @inject(InjectTokens.SoloLogger) private readonly logger: SoloLogger,
-  ) {}
-
   public static getTask(
     k8Factory: K8Factory,
     logger: SoloLogger,
