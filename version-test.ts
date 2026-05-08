dc3e4e059918985c3f03a989bfc4a3a05039f74e
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-05-08 16:43:26 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version-test.ts
+++ version-test.ts
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // using a different version than the one in version.ts to test backwards compatibility
 
-export const TEST_UPGRADE_FROM_VERSION: string = 'v0.71.0';
-export const TEST_UPGRADE_TO_VERSION: string = 'v0.72.0';
+export const TEST_UPGRADE_FROM_VERSION: string = 'v0.72.0';
+export const TEST_UPGRADE_TO_VERSION: string = 'v0.73.0';
 
 // Do not delete, used by test script or Taskfile
 export const PREV_BLOCK_NODE_VERSION: string = 'v0.28.1';
