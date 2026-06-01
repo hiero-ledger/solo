5e82dba9dbf8be20e612961bc0406e55e1c0a5a5
Zhan Milenkov <instamenta@abv.bg>
2026-06-01 16:20:14 +0300

    add labels to solo create namespaces, add logic to differenciate, add logic to delete solo-setup namespace
    Signed-off-by: Zhan Milenkov <instamenta@abv.bg>

--- src/types/namespace/namespaces.ts
+++ src/types/namespace/namespaces.ts
@@ -1,19 +1,28 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import {type NamespaceName} from './namespace-name.js';
+import {type V1Namespace} from '@kubernetes/client-node';
 
 export interface Namespaces {
   /**
    * Create a new namespace
    * @param namespace - the name of the namespace
+   * @param labels - labels to add to namespace
    */
-  create(namespace: NamespaceName): Promise<boolean>;
+  create(namespace: NamespaceName, labels?: Record<string, string>): Promise<boolean>;
 
   /**
    * Delete a namespace
    * @param namespace - the name of the namespace
    */
   delete(namespace: NamespaceName): Promise<boolean>;
+
+  /**
+   * Get a namespace
+   * @param namespace - the name of the namespace
+   * @returns the Kubernetes namespace object
+   */
+  get(namespace: NamespaceName): Promise<V1Namespace>;
 
   /**
    * List all namespaces
