838c16f443c4513e7a5933b4acff0e1b19f8adf0
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-06-30 22:00:34 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- src/commands/one-shot/orchestrator/deploy/default-one-shot-deploy-orchestrator.ts
+++ src/commands/one-shot/orchestrator/deploy/default-one-shot-deploy-orchestrator.ts
@@ -731,10 +731,12 @@ export class DefaultOneShotDeployOrchestrator implements OneShotDeployOrchestrat
                 this.taskList,
                 (): boolean => !getConfig().deployRelay && !getConfig().minimalSetup,
               ),
-          }).withWaitCondition(
-            SoloEventType.MirrorNodeDeployed,
-            Duration.ofMinutes(constants.MIRROR_NODE_DEPLOYED_EVENT_TIMEOUT_MINUTES),
-          ),
+          })
+            .withWaitCondition(
+              SoloEventType.MirrorNodeDeployed,
+              Duration.ofMinutes(constants.MIRROR_NODE_DEPLOYED_EVENT_TIMEOUT_MINUTES),
+            )
+            .withWaitCondition(SoloEventType.NodesStarted, Duration.ofMinutes(10)),
         ],
         (getConfig: () => OneShotSingleDeployConfigClass): ExecutionMode =>
           getConfig().parallelDeploy
