0a4b8f91e0e2dc22098447349348f06943a68bd4
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-07-07 13:58:17 -0500

    new version
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version-test.ts
+++ version-test.ts
@@ -2,7 +2,7 @@
 // using a different version than the one in version.ts to test backwards compatibility
 
 export const TEST_UPGRADE_FROM_VERSION: string = 'v0.74.0';
-export const TEST_UPGRADE_TO_VERSION: string = 'v0.75.0-rc.6';
+export const TEST_UPGRADE_TO_VERSION: string = 'v0.75.0-rc.8';
 
 // Do not delete, used by test script or Taskfile
 export const PREV_BLOCK_NODE_VERSION: string = 'v0.36.0';
