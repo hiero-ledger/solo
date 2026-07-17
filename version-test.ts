b1df17a9fa1bac61783af263382dca9947f2a029
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-07-17 11:07:23 -0500

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version-test.ts
+++ version-test.ts
@@ -5,7 +5,7 @@ export const TEST_UPGRADE_FROM_VERSION: string = 'v0.74.0';
 export const TEST_UPGRADE_TO_VERSION: string = 'v0.75.1';
 
 // Do not delete, used by test script or Taskfile
-export const PREV_BLOCK_NODE_VERSION: string = 'v0.36.0';
-export const PREV_MIRROR_NODE_VERSION: string = 'v0.156.0';
+export const PREV_BLOCK_NODE_VERSION: string = 'v0.37.0';
+export const PREV_MIRROR_NODE_VERSION: string = 'v0.158.0';
 export const PREV_EXPLORER_VERSION: string = '25.1.1';
 export const PREV_RELAY_VERSION: string = '0.76.0';
