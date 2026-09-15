78115e1699d96c4a8fc6dc91f63c9ea059482bde
Fantomasa <ivailoinfo@gmail.com>
2026-09-15 12:41:02 +0300

    fix: cast checkAllNodeProxiesAreActive to the upgrade context so the merged tree compiles
    Signed-off-by: Fantomasa <ivailoinfo@gmail.com>

--- src/commands/node/handlers.ts
+++ src/commands/node/handlers.ts
@@ -470,7 +470,7 @@ export class NodeCommandHandlers extends CommandHandler {
       this.skipTaskWhenNodeStartSkipped(this.tasks.startNodes('allNodeAliases')),
       this.skipTaskWhenNodeStartSkipped(this.tasks.enablePortForwarding()),
       this.skipTaskWhenNodeStartSkipped(this.tasks.checkAllNodesAreActive('allNodeAliases')),
-      this.skipTaskWhenNodeStartSkipped(this.tasks.checkAllNodeProxiesAreActive()),
+      this.skipTaskWhenNodeStartSkipped(this.tasks.checkAllNodeProxiesAreActive() as SoloListrTask<NodeUpgradeContext>),
       this.skipTaskWhenNodeStartSkipped(this.tasks.finalize()),
     ];
   }
