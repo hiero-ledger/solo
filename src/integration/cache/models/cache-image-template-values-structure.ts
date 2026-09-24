35e7116c3fa491f8718ae5d2ece84ce9d7385ced
Ivo Yankov <ivo@devlabs.bg>
2026-09-24 21:46:38 +0300

    fix: substitute minio for pgsty/silo (#6051)
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- src/integration/cache/models/cache-image-template-values-structure.ts
+++ src/integration/cache/models/cache-image-template-values-structure.ts
@@ -6,6 +6,7 @@ export interface CacheImageTemplateValuesStructure {
   readonly RELAY_VERSION: string;
   readonly EXPLORER_VERSION: string;
   readonly MINIO_OPERATOR_VERSION: string;
+  readonly MINIO_IMAGE_DIGEST: string;
   readonly SOLO_CHEETAH_VERSION: string;
   readonly SOLO_CONTAINERS_VERSION: string;
 }
