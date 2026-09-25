e3e5a1bb6b0efc5f1699ef6c9e6a145c70a8b0a3
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-09-25 09:18:55 -0500

    format
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- version.ts
+++ version.ts
@@ -25,8 +25,7 @@ export const AARDVARK_DNS_VERSION: string = 'v2.0.0';
 export const KUBECTL_VERSION: string = 'v1.32.2';
 
 export const SOLO_CHART_VERSION: string = constants.getEnvironmentVariable('SOLO_CHART_VERSION') || '0.66.2';
-export const HEDERA_PLATFORM_VERSION: string =
-  constants.getEnvironmentVariable('CONSENSUS_NODE_VERSION') || 'v0.77.2';
+export const HEDERA_PLATFORM_VERSION: string = constants.getEnvironmentVariable('CONSENSUS_NODE_VERSION') || 'v0.77.2';
 export const MIRROR_NODE_VERSION: string = constants.getEnvironmentVariable('MIRROR_NODE_VERSION') || 'v0.163.0';
 export const EXPLORER_VERSION: string = constants.getEnvironmentVariable('EXPLORER_VERSION') || '26.2.0';
 export const HEDERA_JSON_RPC_RELAY_VERSION: string = constants.getEnvironmentVariable('RELAY_VERSION') || '0.78.1';
